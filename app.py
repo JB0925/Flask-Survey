87e2ab0696870eb634cd85bddfe7361c6a676f25
Jesse Brink <jbrink0925@gmail.com>
2021-07-02 00:54:24 -0400

    users now cannot skip questions

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-from surveys import Question, surveys
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from surveys import surveys
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hahaha'
 
 RESPONSES = []
 main_survey = surveys['satisfaction']
@@ -9,15 +10,21 @@ all_questions = [question.question for question in main_survey.questions]
 answers = [question.choices for question in main_survey.questions]
 
 
+
 @app.route('/', methods=['GET'])
 def start():
+    session.pop('_flashes', None)
     return render_template('start.html', survey=main_survey)
 
 
 @app.route('/questions/<num>', methods=['GET', 'POST'])
 def questions(num):
-
     new_num = int(num)
+    if RESPONSES and new_num != len(RESPONSES) or \
+        not RESPONSES and new_num > 0:
+        flash('Sorry, you can\'t skip questions!', 'error')
+        return redirect(url_for('questions', num=len(RESPONSES) or 0))
+    
     if new_num == len(all_questions):
         return redirect(url_for('thanks'))
 
@@ -32,4 +39,5 @@ def questions(num):
 
 @app.route('/thanks', methods=['GET'])
 def thanks():
+    session.pop('_flashes', None)
     return render_template('thankyou.html')
